c3feffaa2576eab68cecbf9e3dc151aaf8ec0b2c
DPeet <bearbiever@gmail.com>
2014-09-20 19:18:24 +0000

    ima dummy

--- Meteor/HackGTHealthCare/server/HackGTHealthCare.js
+++ Meteor/HackGTHealthCare/server/HackGTHealthCare.js
@@ -19,14 +19,14 @@ Meteor.startup(function () {
 		Hospital.insert({name: "Sutter Medical Center", streetaddress: "3325 Chanate Rd", city: "Santa Rosa", state: "CA", zip: 95404, lat: 38.469572, long: -122.707625});
 		
 		var h = Hospital.find({name: "Grady"}).fetch();
-		console.log(h[]._id);
+		console.log(h[0]._id);
 		
 		Drug.insert({name: "Fentanyl", chemical: "", normalDosage: "", sideEffects: "", personalEffect: "GOOD", personalEffectNotes: "extremely effective" });
 		Drug.insert({name: "Propofol", chemical: "", normalDosage: "", sideEffects: "", personalEffect: "GOOD", personalEffectNotes: "reacted wel"});
 		Drug.insert({name: "Ketamine", chemical: "", normalDosage: "", sideEffects: "", personalEffect: "BAD", personalEffectNotes: "caused drug induced psychosis.  DO NOT TAKE"});
 		
 		var d = Drug.find({name: "Fentanyl"}).fetch();
-		console.log(d[]._id);
+		console.log(d[0]._id);
 		
 		MajorEvent.insert({time: "1 pm", date: "3/3/2014", hospitalId: h[0]._id, otherLocation: "In the Grady trauma ER they performed the reduction on my right shoulder back in.  Afterwards both my left and right shoulders would sublux", tags: "Shoulder", notes: "", drugs: d[0]._id+""});
 		MajorEvent.insert({time: "5 pm", date: "5/3/2014", hospitalId: h[0]._id, otherLocation: "They put your shoulder back in at Gwinnett Medical Center - Duluth ER.", tags: "Shoulder", notes: "", drugs: d[0]._id+""});
